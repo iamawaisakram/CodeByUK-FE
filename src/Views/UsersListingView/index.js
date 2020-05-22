d7a50069d732a67eb2d2cd8ec46010af7b4b8898
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-23 04:10:29 +0500

    User Listing Completed

--- src/Views/UsersListingView/index.js
+++ src/Views/UsersListingView/index.js
@@ -4,6 +4,9 @@ import { useRecoilState } from 'recoil'
 // * Atoms and Selectors
 import { AllUsersAtom } from '../../Recoil/User/Atoms'
 
+// * API calls
+import { GetAllUsersAPI } from '../../Api/User'
+
 // * Style
 import './index.css'
 
@@ -11,6 +14,34 @@ export default props => {
   let [allUsers, setAllUsersHandler] = useRecoilState(AllUsersAtom)
   const [searchName, setSearchName] = useState('')
   const [searchId, setSearchId] = useState('')
+
+  const searchUserHandler = () => {
+    let users = []
+    if (searchName) {
+      allUsers.map(user => {
+        let name = user.firstname.concat(' ', user.lastname)
+        name = name.toLowerCase()
+        if (name.includes(searchName.toLowerCase())) {
+          if (!users.includes(user)) users.push(user)
+        }
+      })
+    }
+    if (searchId) {
+      allUsers.map(user => {
+        if (user.id == searchId) {
+          if (!users.includes(user)) users.push(user)
+        }
+      })
+    }
+    setAllUsersHandler(users)
+  }
+
+  const resetHandler = async () => {
+    setSearchName('')
+    setSearchId('')
+    let result = await GetAllUsersAPI()
+    setAllUsersHandler(result)
+  }
 
   return (
     <div id='users-listing'>
@@ -23,13 +54,18 @@ export default props => {
           onChange={e => setSearchName(e.target.value)}
         />
         <input
-          type='text'
+          type='number'
           value={searchId}
           className='input'
           placeholder='Search By Id'
           onChange={e => setSearchId(e.target.value)}
         />
-        <button className='submit-btn'>Submit</button>
+        <button onClick={() => searchUserHandler()} className='submit-btn'>
+          Submit
+        </button>
+        <button onClick={() => resetHandler()} className='reset-btn'>
+          Reset
+        </button>
       </div>
       <div className='users-listing-result'>
         {allUsers.map(entry => (
