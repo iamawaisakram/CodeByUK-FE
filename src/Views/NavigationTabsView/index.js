bb51a041195cd85dad9e249c4530205f5269347d
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-22 21:57:36 +0500

    Add Logout Button

--- src/Views/NavigationTabsView/index.js
+++ src/Views/NavigationTabsView/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default props => {
-  return <div id='topbar'>Top Bar</div>
+  let history = useHistory()
+  const handleLogout = () => {
+    localStorage.setItem('token', '')
+    history.push('/login')
+  }
+  return (
+    <div id='topbar'>
+      <button className='logout' onClick={() => handleLogout()}>
+        Logout
+      </button>{' '}
+    </div>
+  )
 }
