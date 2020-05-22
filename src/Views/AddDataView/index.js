d2ed41b50ae7d9c4c6548c3d808933341acd35d8
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-23 01:55:43 +0500

    Add Data Entry Values Complete

--- src/Views/AddDataView/index.js
+++ src/Views/AddDataView/index.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // * style
 import './index.css'
+import { AddDataAPI, GetTestResultsAPI } from '../../Api/Data'
+import { useSetRecoilState } from 'recoil'
+import { DataEntriesAtom, TestResultsAtom } from '../../Recoil/Data/Atoms'
 
 export default props => {
-  return <div id='add-data'>Add Data</div>
+  const [dataEntry, setDataEntry] = useState(0)
+  const [logbookEntry, setLogbookEntry] = useState('')
+  let setTestResults = useSetRecoilState(TestResultsAtom)
+
+  const setDataEntryValues = useSetRecoilState(DataEntriesAtom)
+
+  const handleSubmit = async () => {
+    let result = await AddDataAPI({ value: dataEntry, logbookEntry })
+    setDataEntryValues(result)
+    setTestResults(await GetTestResultsAPI())
+  }
+
+  return (
+    <div id='add-data'>
+      <div className='title-div'>
+        <p className='title'>Add Data</p>
+      </div>
+      <div className='body-div'>
+        <input
+          type='number'
+          className='input'
+          value={dataEntry}
+          placeholder='Value'
+          onChange={e => setDataEntry(e.target.value)}
+        />
+        <input
+          type='text'
+          className='input'
+          value={logbookEntry}
+          placeholder='Logbook Entry'
+          onChange={e => setLogbookEntry(e.target.value)}
+        />
+        <button onClick={() => handleSubmit()} className='submit-btn'>
+          Add Value
+        </button>
+      </div>
+    </div>
+  )
 }
