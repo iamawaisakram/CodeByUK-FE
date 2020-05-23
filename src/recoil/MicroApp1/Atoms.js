346f24aceff0314848e449f6973445eb094b3055
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-05-23 14:46:03 +0500

    Delete Atoms.js

--- src/recoil/MicroApp1/Atoms.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil'
-
-export const ViewRenderAtom = atom({
-  key: 'viewRender', // * unique ID (with respect to other atoms/selectors)
-  default: 'home' // * default value (aka initial value)
-})
